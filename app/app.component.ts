3a678b43ff5f61e573ed45d7330c59c90f4b21d8
dgabel001 <duane_gabel@cable.comcast.com>
2018-12-17 17:24:16 -0700

    02_06b

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  // styles: [`
+  //   h1 { color: #ffffff;}
+  //   .description {
+  //     font-style: italic;
+  //     color: green;
+  //   }
+  // `],
+  styleUrls: ['app/app.component.css']
 })
 export class AppComponent {}
